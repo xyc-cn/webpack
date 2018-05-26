d840cff3f1c627b231e92e1e6d820266a7422593
easonxie <easonxie@tencent.com>
2018-05-26 11:30:42 +0800

    init

--- template/src/pages/module/index/v_entry.js
+++ template/src/pages/module/index/v_entry.js
@@ -1,26 +1,15 @@
-{{#if_eq build "standalone"}}
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-{{/if_eq}}
 import Vue from 'vue'
-import App from './App'
-{{#router}}
+import App from './v_entry.vue'
 import router from './router'
-{{/router}}
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  {{#router}}
+  el: '#root',
   router,
-  {{/router}}
-  {{#if_eq build "runtime"}}
-  render: h => h(App)
-  {{/if_eq}}
-  {{#if_eq build "standalone"}}
   components: { App },
   template: '<App/>'
-  {{/if_eq}}
 })
